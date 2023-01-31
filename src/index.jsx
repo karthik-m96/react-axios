bddb46e78cf7d8469092de2fe81b5d4d37c48574
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-01-31 11:03:24 +0530

    change

--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,8 @@ import Table from './Table.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <App />
     <Table  />
-  </React.StrictMode>
+  </>
 );
